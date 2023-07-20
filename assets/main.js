d73858e5101a96d2637fef9559909e4b4213d936
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2023-07-20 20:57:38 -0300

    responsivo e validação funfante

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,10 @@ const atualAno = dataAtual.getFullYear();
 const atualMes = dataAtual.getMonth();
 const atualDia = dataAtual.getDate();
 
+const mesCom31dias = [1,3,5,7,8,10,12];
+const mesSem31dias = [2,4,6,9,11];
+
+
 
 const botao = document.querySelector('.botao');
 const ano = document.querySelector('#ano');
@@ -30,22 +34,14 @@ botao.addEventListener('click', () => {
 
 
 
-    console.log(idDia);
-    console.log(idMes);
-    console.log(idAno);
-
-    console.log(atualAno);
-
 /* validação */
-
-
     const border = document.querySelectorAll('.date');
     const vazios = document.querySelectorAll('.descricao');
 
     const errorRequired = document.querySelectorAll('#required');
-    const ErrorPast = document.querySelector('#erPast');
-    const ErrorMonth = document.querySelector('#erMonth');
-    const ErrorDay = document.querySelector('#erDay');
+    const errorPast = document.querySelector('#erPast');
+    const errorMonth = document.querySelector('#erMonth');
+    const errorDay = document.querySelector('#erDay');
 
 
     for (let i = 0; i < border.length; i++) {
@@ -54,41 +50,68 @@ botao.addEventListener('click', () => {
         const required = errorRequired[i];
         const descricao = vazios[i];
 
-         if (border[0].value > 31) {
-            descricao.innerHTML= vazio;
-            border[0].style.boxShadow = '0 0 5px red';
+        if (border[i].value === '') {
+            descricao.innerHTML = vazio;
+            border[i].style.boxShadow = '0 0 5px red';
             botao.style.backgroundImage = 'url("./assets/images/button.png")';
-            ErrorDay.style.display = 'flex';
-        }else if (border[1].value > 12) {
-            descricao.innerHTML= vazio;
-            border[1].style.boxShadow = '0 0 5px red';
-            botao.style.backgroundImage = 'url("./assets/images/button.png")';
-            ErrorMonth.style.display = 'flex';
-
-        }else if (border[2].value > atualAno) {
-            descricao.innerHTML= vazio;
-            border[2].style.boxShadow = '0 0 5px red';
-            botao.style.backgroundImage = 'url("./assets/images/button.png")';
-            ErrorPast.style.display = 'flex';
-          
-        }else if (element === '') {
-            descricao.innerHTML= vazio;
-            date.style.boxShadow = '0 0 5px red';
-            botao.style.backgroundImage = 'url("./assets/images/button.png")';
-            required.style.display = 'flex';
-
-        }else{
+            errorRequired[i].style.display = 'flex';
+        } else{
             date.style.boxShadow = '0 0 0px red';
             required.style.display = 'none';
-            ErrorPast.style.display = 'none';
-            ErrorMonth.style.display = 'none';
-            ErrorDay.style.display = 'none';
+
+        switch (i) {
+            case 0:
+                if (border[0].value > 30 && mesSem31dias.includes(Number(border[1].value)) || border[0].value > 31) {
+                    descricao.innerHTML = vazio;
+                    border[0].style.boxShadow = '0 0 5px red';
+                    botao.style.backgroundImage = 'url("./assets/images/button.png")';
+                    errorDay.style.display = 'flex';
+                } else {
+                    date.style.boxShadow = '0 0 0px red';
+                    errorDay.style.display = 'none';
+                }
+                break;
+            case 1:
+                if (border[1].value > 12) {
+                    descricao.innerHTML = vazio;
+                    border[1].style.boxShadow = '0 0 5px red';
+                    botao.style.backgroundImage = 'url("./assets/images/button.png")';
+                    errorMonth.style.display = 'flex';
+                } else {
+                    date.style.boxShadow = '0 0 0px red';
+                    errorMonth.style.display = 'none';
+                }
+                break;
+            case 2:
+                if (border[2].value > atualAno) {
+                    descricao.innerHTML = vazio;
+                    border[2].style.boxShadow = '0 0 5px red';
+                    botao.style.backgroundImage = 'url("./assets/images/button.png")';
+                    errorPast.style.display = 'flex';
+                } else {
+                    date.style.boxShadow = '0 0 0px red';
+                    errorPast.style.display = 'none';
+                }
+                break;
+        }
+        if (errorPast.style.display == 'none' && errorMonth.style.display == 'none' && errorDay.style.display == 'none') {
+            errorPast.style.display = 'none';
+            errorMonth.style.display = 'none';
+            errorDay.style.display = 'none';
             botao.style.backgroundImage = 'url("./assets/images/butonblack.png")';
             resultAno.innerHTML = resultIdAno;
             resultMes.innerHTML = resultIdMes;
             resultDia.innerHTML = resultIdDia;
+        }else{
+            resultAno.innerHTML = vazio;
+            resultMes.innerHTML = vazio;
+            resultDia.innerHTML = vazio;
         }
 
+        console.log(mesSem31dias.length.val);
+
+        
+        }
     }
 
 
